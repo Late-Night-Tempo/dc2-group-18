386397be9ef76bb76eaaf03bdac2da7a853a4ad9
20221133 <a.denaughel@student.tue.nl>
2024-04-25 21:53:57 +0200

    Initial commit, adds a main.py to work with, adds a .gitignore to save us directory issues, and added a requirements.txt. The gitignore in the .idea should be fine alongside the one in the main folder.

---
+++ main.py
@@ -0,0 +1,2 @@
+print("This is the first commit with a test print. If you are having issues, please let the group know.")
+print("Before doing any commits to main, please remember to clear it with the group!")
