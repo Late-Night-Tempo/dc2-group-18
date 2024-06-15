1056ce4bcb70a43605b715c0536c0dc3b2884927
20221133 <a.denaughel@student.tue.nl>
2024-06-15 17:04:30 +0200

    Added MSE and MAE

--- 3d_stat_sheet.py
+++ 3d_stat_sheet.py
@@ -19,15 +19,16 @@ import plotly.express as px
 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+
 
 # Collects the smaller year from the below llw database
 def year_min(input_string):
-
     # Adjusted pattern to capture the year
     pattern = r'(\d{4})'
 
@@ -42,9 +43,9 @@ def year_min(input_string):
     else:
         return None
 
+
 # Collects the year as is from a string/column
 def year_norm(input_string):
-
     # Adjusted pattern to capture the year
     pattern = r'(\d{4})'
 
@@ -58,7 +59,6 @@ def year_norm(input_string):
         return year_max
     else:
         return None
-
 
 
 # Collect dfs
@@ -132,17 +132,17 @@ response_numerical = {
 
 }
 
-#question_list = ["NQ135BD", "NQ21", "Q61", "Q62A", "Q62TI"]
-question_list = ["C2;3", "NQ135BD;377", "NQ21;55", "Q61;97", "Q62A;98", "Q62TI;106", "MONTH_YEAR", "MONTH", "YEAR"]
+# question_list = ["NQ135BD;377", "NQ21;55", "Q61;97", "Q62A;98", "Q62TI;106",]
+# question2 = ["Q62B;99", "Q62C;100", "Q62H;105", "Q62E;102", "Q60;96"]
+question_list = ["C2;3", "Q62B;99", "Q62C;100", "Q62H;105", "Q62E;102", "Q60;96", "MONTH_YEAR", "MONTH", "YEAR"]
 
- # Collect month + year
+# Collect month + year
 pas_ward["MONTH_YEAR"] = pas_ward["MONTH"].str.extract(r'\(([^)]+)\)')
 pas_ward["YEAR"] = pd.to_datetime(pas_ward["MONTH_YEAR"]).dt.year
 
 # Set pas_ward to only questions in question list
 pas_ward = pas_ward[question_list]
 pas_ward.rename(columns={"C2;3": "Area Name"}, inplace=True)
-
 
 ## Begin attatching other datasets to current dataset
 
@@ -183,27 +183,22 @@ for columns in week_income.columns:
     if "Unnamed" in columns or "Code" in columns:
         week_income.drop(columns=[columns], inplace=True)
 
-
 # remove "pay" listings and city of london
 week_income = week_income.truncate(before=3, after=34)
 econ_inac_fem = econ_inac_fem.truncate(before=1)
 econ_inac_mal = econ_inac_mal.truncate(before=1)
 
-
-
-
 # Add proportion. proportion shall be our lovely y variable. the questions will indicate the predicting vars
 # Although we will need to separate the measures... make multiple models, one per each measure!
 trust_merge = trust_df.drop(columns=["Unnamed: 0", "Survey", "MPS"])
-trust_merge.rename(columns={"Borough": "Area Name", "Date":"YEAR"}, inplace=True)
+trust_merge.rename(columns={"Borough": "Area Name", "Date": "YEAR"}, inplace=True)
 trust_merge["YEAR"] = pd.to_datetime(trust_merge["YEAR"])
 trust_merge["YEAR"] = trust_merge["YEAR"].dt.year
-
 
 # Now all the column names are just the year, yippee
 # Now we match borough and year together, and create LLW and Percent inactivity column
 
- # Collect month + year
+# Collect month + year
 pas_ward["MONTH_YEAR"] = pas_ward["MONTH"].str.extract(r'\(([^)]+)\)')
 pas_ward["YEAR"] = pd.to_datetime(pas_ward["MONTH_YEAR"]).dt.year
 
@@ -211,34 +206,28 @@ pas_ward["YEAR"] = pd.to_datetime(pas_ward["MONTH_YEAR"]).dt.year
 df_B_long = below_llw.melt(id_vars=['Area Name'], var_name='YEAR', value_name='LLW')
 df_B_long['YEAR'] = df_B_long['YEAR'].astype(int)
 
-
 # Reshape DataFrame Inactivity fem from wide to long format
 df_inac_long_fem = econ_inac_fem.melt(id_vars=['Area'], var_name='YEAR', value_name='Inac_Perc_Fem')
 df_inac_long_fem.rename(columns={"Area": "Area Name"}, inplace=True)
 df_inac_long_fem['YEAR'] = df_inac_long_fem['YEAR'].astype(int)
-
 
 # Reshape DataFrame Inactivity mal from wide to long format
 df_inac_long_mal = econ_inac_mal.melt(id_vars=['Area'], var_name='YEAR', value_name='Inac_Perc_Mal')
 df_inac_long_mal.rename(columns={"Area": "Area Name"}, inplace=True)
 df_inac_long_mal['YEAR'] = df_inac_long_mal['YEAR'].astype(int)
 
-
 # Reshape DataFrame Weekly Income from wide to long format
 df_week_inc_long = week_income.melt(id_vars=['Area'], var_name='YEAR', value_name='Week_Inc')
 df_week_inc_long.rename(columns={"Area": "Area Name"}, inplace=True)
 df_week_inc_long['YEAR'] = df_week_inc_long['YEAR'].astype(int)
 
-
 # Merge DataFrame Pas and LLW
 pas_merged_incom = pd.merge(pas_ward, df_B_long, on=['Area Name', 'YEAR'])
-
 
 # Merge pas and inac
 pas_logistic = pd.merge(pas_merged_incom, df_inac_long_fem, on=["Area Name", "YEAR"])
 pas_logistic = pd.merge(pas_logistic, df_inac_long_mal, on=["Area Name", "YEAR"])
 pas_logistic = pd.merge(pas_logistic, df_week_inc_long, on=["Area Name", "YEAR"])
-
 
 # Merge pas and trust
 
@@ -247,7 +236,8 @@ pas_logistic = pd.merge(pas_logistic, df_week_inc_long, on=["Area Name", "YEAR"]
 trust_merge.drop_duplicates(inplace=True)
 
 # Use pivot_table to pivot the DataFrame
-df_pivot = trust_merge.pivot_table(index=['YEAR', 'Area Name'], columns='Measure', values='Proportion', aggfunc='first').reset_index()
+df_pivot = trust_merge.pivot_table(index=['YEAR', 'Area Name'], columns='Measure', values='Proportion',
+                                   aggfunc='first').reset_index()
 
 # Flatten the columns
 df_pivot.columns.name = None
@@ -259,15 +249,17 @@ df_pivot.reset_index(inplace=True, drop=True)
 # Merge new pivot with proportion data into pas_logistic
 pas_logistic = pd.merge(pas_logistic, df_pivot, on=["Area Name", "YEAR"])
 
-#print(pas_logistic)
+# print(pas_logistic)
 pas_logistic.to_csv("LregPasData.csv", index=False)
 
 # Set targets and inputs
 target_category = "Trust MPS"
-limited_global_cols = ["YEAR","LLW","Inac_Perc_Fem","Inac_Perc_Mal","Week_Inc", target_category]
-input_cols = ["YEAR","LLW","Inac_Perc_Fem","Inac_Perc_Mal","Week_Inc"]
-all_global_cols = ["YEAR","LLW","Inac_Perc_Fem","Inac_Perc_Mal","Week_Inc", "NQ135BD;377", "NQ21;55", "Q61;97", "Q62A;98", "Q62TI;106", target_category]
-input_and_q_cols = ["YEAR","LLW","Inac_Perc_Fem","Inac_Perc_Mal","Week_Inc", "NQ135BD;377", "NQ21;55", "Q61;97", "Q62A;98", "Q62TI;106"]
+limited_global_cols = ["YEAR", "LLW", "Inac_Perc_Fem", "Inac_Perc_Mal", "Week_Inc", target_category]
+input_cols = ["YEAR", "LLW", "Inac_Perc_Fem", "Inac_Perc_Mal", "Week_Inc"]
+all_global_cols = ["YEAR", "LLW", "Inac_Perc_Fem", "Inac_Perc_Mal", "Week_Inc", "Q62B;99", "Q62C;100", "Q62H;105",
+                   "Q62E;102", "Q60;96", target_category]
+input_and_q_cols = ["YEAR", "LLW", "Inac_Perc_Fem", "Inac_Perc_Mal", "Week_Inc", "Q62B;99", "Q62C;100", "Q62H;105",
+                    "Q62E;102", "Q60;96"]
 
 # Filter to include only the current categories and target category. Drop nans.
 pas_logistic_filtered = pas_logistic[limited_global_cols].copy().dropna(subset=[target_category])
@@ -292,6 +284,14 @@ coefficients = pd.Series(model.coef_, index=X.columns)
 largest_abs_coefficient = coefficients.abs().idxmax()
 largest_abs_value = coefficients[largest_abs_coefficient]
 
+# Set predictions and results for model 1
+y_pred = model.predict(X_test)
+
+
+X_test["Actual"] = y_test
+X_test["Predicted"] = y_pred
+
+
 # Print details of the linear regression model
 print("GLOBAL BASIC REGRESSION")
 print(f"Category: {input_cols}")
@@ -301,15 +301,23 @@ print(f"The coefficient value is: {largest_abs_value}")
 print("Intercept:", model.intercept_)
 print("Coefficients:", model.coef_)
 print("R-squared:", model.score(X, y))
+print("MSE:", mean_squared_error(X_test["Predicted"], X_test["Actual"]))
+print("MAE:", mean_absolute_error(X_test["Predicted"], X_test["Actual"]))
 print()
+
 
 # Drop rows with NaN in the target category
 pas_logistic_question_filtered = pas_logistic[all_global_cols].copy().dropna(subset=[target_category])
 
+
+## BEGIN MODEL 2 SETTING
+
 # Encode categorical variables using OrdinalEncoder
 ordinal_encoder = OrdinalEncoder()
-categorical_columns = ["NQ135BD;377", "NQ21;55", "Q61;97", "Q62A;98", "Q62TI;106"]
-pas_logistic_question_filtered[categorical_columns] = ordinal_encoder.fit_transform(pas_logistic_question_filtered[categorical_columns].astype(str))
+categorical_columns = ["Q62B;99", "Q62C;100", "Q62H;105", "Q62E;102", "Q60;96"]
+pas_logistic_question_filtered[categorical_columns] = ordinal_encoder.fit_transform(
+    pas_logistic_question_filtered[categorical_columns].astype(str))
+pas_logistic_question_filtered["Area Name"] = pas_logistic["Area Name"]
 
 # Set X and y
 input_and_q_cols = [col for col in all_global_cols if col != target_category]
@@ -332,6 +340,31 @@ coefficients2 = pd.Series(model2.coef_, index=X2.columns)
 largest_abs_coefficient2 = coefficients2.abs().idxmax()
 largest_abs_value2 = coefficients2[largest_abs_coefficient2]
 
+# Combine the actual and predicted values into a DataFrame for visualization
+results_df = pd.DataFrame({
+    'Actual': y_test,
+    'Predicted': y_pred
+})
+
+# Add the test features to the results for further analysis
+results_df = pd.concat([results_df, X2_test.reset_index(drop=True)], axis=1)
+
+
+y2_pred = model2.predict(X2_test)
+
+# Combine the actual and predicted values into a DataFrame for visualization
+results_df2 = pd.DataFrame({
+    'Actual': y2_test,
+    'Predicted': y2_pred
+})
+
+# Add the test features to the results for further analysis
+results_df2 = pd.concat([results_df2, X2_test.reset_index(drop=True)], axis=1)
+
+
+X2_test["Actual"] = y2_test
+X2_test["Predicted"] = y2_pred
+
 # Print details of the linear regression model
 print("GLOBAL QUESTIONS ADDED REGRESSION")
 print(f"Category: {input_and_q_cols}")
@@ -341,104 +374,162 @@ print(f"The coefficient value is: {largest_abs_value2}")
 print("Intercept:", model2.intercept_)
 print("Coefficients:", model2.coef_)
 print("R-squared:", model2.score(X2, y2))
-print()
+print("MSE:", mean_squared_error(X2_test["Predicted"], X2_test["Actual"]))
+print("MAE:", mean_absolute_error(X2_test["Predicted"], X2_test["Actual"]))
+
 
 
 
 # Find borough with the most outliers
 
 
-
 # Graph the lines
 
 
 ############## MODEL 1 #########################
-y_pred = model.predict(X_test)
-
-# Combine the actual and predicted values into a DataFrame for visualization
-results_df = pd.DataFrame({
-    'Actual': y_test,
-    'Predicted': y_pred
-})
-
-# Add the test features to the results for further analysis
-results_df = pd.concat([results_df, X_test.reset_index(drop=True)], axis=1)
 
 
-plt.figure(figsize=(10, 6))
-sns.scatterplot(x=results_df['Actual'], y=results_df['Predicted'])
+plt.figure(figsize=(10, 10))
+sns.scatterplot(x=results_df['Actual'], y=results_df['Predicted'],  s=50, alpha=0.4)
 plt.plot([results_df['Actual'].min(), results_df['Actual'].max()],
          [results_df['Actual'].min(), results_df['Actual'].max()],
-         color='red', linestyle='--')
-plt.xlabel('Actual')
-plt.ylabel('Predicted')
-plt.title(f'Model 1 Actual vs. Predicted')
+         color='chocolate', linestyle='--', linewidth=2)
+plt.xlabel('Actual "Trust in MPS" Proportion', weight='bold', size=16)
+plt.ylabel('Predicted "Trust in MPS" Proportion', weight='bold', size=16)
+plt.title(f'Trust in the MPS as a Result of Socioeconomic Factors:\n Actual vs. Predicted', weight='bold', size=20)
+# Title - bold and size 20
+# Axis title - bold and size 16
+# Axis ticks - bold and size 16
+# Legend - title = 16 text = 14
+plt.xticks(fontsize=16, weight='bold')
+plt.yticks(fontsize=16, weight='bold')
+plt.legend(labels=["Results from Dataset", "Ideal Baseline"], fontsize=14, title_fontsize=16)
+plt.tight_layout()
 plt.show()
 
-# plt.figure(figsize=(10, 6))
-# sns.scatterplot(x=results_df[feature_to_plot], y=results_df['Actual'], label='Actual')
-# sns.lineplot(x=results_df[feature_to_plot], y=results_df['Predicted'], color='red', label='Predicted')
-# plt.xlabel(feature_to_plot)
-# plt.ylabel('Target')
-# plt.title(f'{feature_to_plot} vs. Target with Regression Line')
-# plt.legend()
-# plt.show()
-#
-# # Calculate residuals
-# results_df['Residuals'] = results_df['Actual'] - results_df['Predicted']
-#
-# plt.figure(figsize=(10, 6))
-# sns.scatterplot(x=results_df['Predicted'], y=results_df['Residuals'])
-# plt.axhline(0, color='red', linestyle='--')
-# plt.xlabel('Predicted')
-# plt.ylabel('Residuals')
-# plt.title('Residuals vs. Predicted')
-# plt.show()
+# Calculate residuals
+results_df['Residuals'] = results_df['Actual'] - results_df['Predicted']
 
-
-
+plt.figure(figsize=(10, 10))
+sns.scatterplot(x=results_df['Predicted'], y=results_df['Residuals'],  s=50, alpha=0.4)
+plt.axhline(0, color='chocolate', linestyle='--', linewidth=2)
+plt.xlabel('Predicted "Trust in MPS" Proportion' , weight='bold', size=16)
+plt.ylabel('Residuals of "Trust in MPS" Proportion', weight='bold', size=16)
+plt.title('Trust in the MPS as a Result of Socioeconomic Factors:\n Residuals vs. Predicted', weight='bold', size=20)
+plt.legend(labels=["Results from Dataset", "Ideal Baseline"], fontsize=14, title_fontsize=16)
+plt.xticks(fontsize=16, weight='bold')
+plt.yticks(fontsize=16, weight='bold')
+plt.tight_layout()
+plt.show()
 
 ################## MODEL 2 ###################
 
-y2_pred = model2.predict(X2_test)
 
-# Combine the actual and predicted values into a DataFrame for visualization
-results_df = pd.DataFrame({
-    'Actual': y2_test,
-    'Predicted': y2_pred
-})
 
-# Add the test features to the results for further analysis
-results_df = pd.concat([results_df, X2_test.reset_index(drop=True)], axis=1)
-
-plt.figure(figsize=(10, 6))
-sns.scatterplot(x=results_df['Actual'], y=results_df['Predicted'])
-plt.plot([results_df['Actual'].min(), results_df['Actual'].max()],
-         [results_df['Actual'].min(), results_df['Actual'].max()],
-         color='red', linestyle='--')
-plt.xlabel('Actual')
-plt.ylabel('Predicted')
-plt.title('Model 2 Actual vs. Predicted')
+plt.figure(figsize=(10, 10))
+sns.scatterplot(x=results_df2['Actual'], y=results_df2['Predicted'],  s=50, alpha=0.4)
+plt.plot([results_df2['Actual'].min(), results_df2['Actual'].max()],
+         [results_df2['Actual'].min(), results_df2['Actual'].max()],
+         color='chocolate', linestyle='--', linewidth=2)
+plt.legend(labels=["Results from Dataset", "Ideal Baseline"], fontsize=14, title_fontsize=16)
+plt.xlabel('Actual "Trust in MPS" Proportion', weight='bold', size=16)
+plt.ylabel('Predicted "Trust in MPS" Proportion', weight='bold', size=16)
+plt.title('Trust in the MPS as a Result of Socioeconomic Factors\n and Five PAS Survey Questions: Actual vs. Predicted', weight='bold', size=20)
+plt.xticks(fontsize=16, weight='bold')
+plt.yticks(fontsize=16, weight='bold')
+plt.tight_layout()
 plt.show()
 
-# plt.figure(figsize=(10, 6))
-# sns.scatterplot(x=results_df[feature_to_plot], y=results_df['Actual'], label='Actual')
-# sns.lineplot(x=results_df[feature_to_plot], y=results_df['Predicted'], color='red', label='Predicted')
-# plt.xlabel(feature_to_plot)
-# plt.ylabel('Target')
-# plt.title(f'{feature_to_plot} vs. Target with Regression Line')
-# plt.legend()
-# plt.show()
-#
-# # Calculate residuals
-# results_df['Residuals'] = results_df['Actual'] - results_df['Predicted']
-#
-# plt.figure(figsize=(10, 6))
-# sns.scatterplot(x=results_df['Predicted'], y=results_df['Residuals'])
-# plt.axhline(0, color='red', linestyle='--')
-# plt.xlabel('Predicted')
-# plt.ylabel('Residuals')
-# plt.title('Residuals vs. Predicted')
-# plt.show()
+
+# Calculate residuals
+results_df2['Residuals'] = results_df2['Actual'] - results_df2['Predicted']
 
 
+plt.figure(figsize=(10, 10))
+sns.scatterplot(x=results_df2['Predicted'], y=results_df2['Residuals'],  s=50, alpha=0.4)
+plt.axhline(0, color='chocolate', linestyle='--', linewidth=2)
+plt.xlabel('Predicted "Trust in MPS" Proportion', weight='bold', size=16)
+plt.ylabel('Residuals "Trust in MPS" Proportion', weight='bold', size=16)
+plt.title('Trust in the MPS as a Result of Socioeconomic Factors\n and Five PAS Survey Questions: Residuals vs. Predicted', weight='bold', size=20)
+plt.legend(labels=["Results from Dataset", "Ideal Baseline"], fontsize=14, title_fontsize=16)
+plt.xticks(fontsize=16, weight='bold')
+plt.yticks(fontsize=16, weight='bold')
+plt.tight_layout()
+plt.show()
+
+##### PER BOROUGH #######
+#
+# for boroughs in pas_logistic_question_filtered["Area Name"].unique()[3:6]:
+#     grouped_df = pas_logistic_question_filtered[pas_logistic_question_filtered["Area Name"] == boroughs].dropna(
+#         subset=[target_category])
+#
+#     # Set X and y
+#     input_and_q_cols = [col for col in all_global_cols if col != target_category]
+#     X2 = grouped_df[input_and_q_cols]
+#     y2 = grouped_df[target_category]
+#
+#     # Train-test split
+#     # use random_state to act as a seed, if needed
+#     # test_size defaults to .25, so 25% of data is used as test data
+#     X2_train, X2_test, y2_train, y2_test = train_test_split(X2, y2)
+#
+#     # Create and train the linear regression model
+#     model2 = LinearRegression()
+#     model2.fit(X2_train, y2_train)
+#
+#     # Extract coefficients and corresponding feature names
+#     coefficients2 = pd.Series(model2.coef_, index=X2.columns)
+#
+#     # Identify the category with the largest absolute value coefficient
+#     largest_abs_coefficient2 = coefficients2.abs().idxmax()
+#     largest_abs_value2 = coefficients2[largest_abs_coefficient2]
+#     y2_pred = model2.predict(X2_test)
+#
+#     # Combine the actual and predicted values into a DataFrame for visualization
+#     results_df = pd.DataFrame({
+#         'Actual': y2_test,
+#         'Predicted': y2_pred
+#     })
+#
+#     # Add the test features to the results for further analysis
+#
+#     X2_test["Actual"] = y2_test
+#     X2_test["Predicted"] = y2_pred
+#
+#     # print(X2_test["Actual"].unique())
+#     # print(X2_test["Predicted"].unique())
+#     # print(len(X2_train), len(X2_test), len(y2_train), len(y2_test))
+#
+#     plt.figure(figsize=(10, 10))
+#     sns.scatterplot(x=X2_test['Actual'], y=X2_test['Predicted'])
+#     plt.plot([X2_test['Actual'].min(), X2_test['Actual'].max()],
+#              [X2_test['Actual'].min(), X2_test['Actual'].max()],
+#              color='red', linestyle='--')
+#     plt.xlabel('Actual')
+#     plt.ylabel('Predicted')
+#     plt.title(f'Model {boroughs} Actual vs. Predicted')
+#     #plt.show()
+#
+#     # Calculate residuals
+#     X2_test['Residuals'] = X2_test['Actual'] - X2_test['Predicted']
+#
+#     # Plotting
+#     plt.figure(figsize=(10, 6))
+#     sns.scatterplot(x=X2_test['Predicted'], y=X2_test['Residuals'])
+#     plt.axhline(0, color='red', linestyle='--')
+#     plt.xlabel('Predicted')
+#     plt.ylabel('Residuals')
+#     plt.title(f'Residuals {boroughs}  vs. Predicted')
+#     #plt.show()
+#
+#     # Print details of the linear regression model
+#     print(f"Per borough {boroughs} ADDED REGRESSION")
+#     print(f"Category: {input_and_q_cols}")
+#     print(f"Target Category: {target_category}")
+#     print(f"The category with the largest absolute value coefficient is: {largest_abs_coefficient2}")
+#     print(f"The coefficient value is: {largest_abs_value2}")
+#     print("Intercept:", model2.intercept_)
+#     print("Coefficients:", model2.coef_)
+#     print("R-squared:", model2.score(X2, y2))
+#     print("MSE:", mean_squared_error(X2_test["Predicted"], X2_test["Actual"]))
+#     print("MAE:", mean_absolute_error(X2_test["Predicted"], X2_test["Actual"]))
